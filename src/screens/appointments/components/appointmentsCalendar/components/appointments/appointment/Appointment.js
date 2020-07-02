92da713356a42ea497391b97afc773948d209392
Profir Oana <oannna1234@gmail.com>
2020-07-02 13:54:40 +0300

    opened approve modal when pressing on pending appointment

--- src/screens/appointments/components/appointmentsCalendar/components/appointments/appointment/Appointment.js
+++ src/screens/appointments/components/appointmentsCalendar/components/appointments/appointment/Appointment.js
@@ -1,24 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TouchableOpacity } from 'react-native';
 
 import { getFirstPicture } from '@@store/modules/services/selectors';
+import * as C from '@@utils/constants';
+import { withOpenModal } from '@@hocs';
 
 import { AppointmentInfo } from './components';
 import * as S from './styled';
 
-const Appointment = ({ appointment = {} }) => (
-  <S.CardContainer>
-    <S.AppointmentInfo>
-      <AppointmentInfo appointment={appointment} />
-    </S.AppointmentInfo>
-    <S.ServicePicture
-      source={{ uri: getFirstPicture(appointment.service || {}) }}
-    />
-  </S.CardContainer>
+const Appointment = ({ appointment = {}, openModal }) => (
+  <TouchableOpacity
+    {...{
+      ...(appointment.status === C.APPOINTMENT_STATUS.PENDING_APPROVAL && {
+        onPress: () =>
+          openModal({
+            name: C.MODALS.APPROVE_APPOINTMENT_MODAL,
+            data: {
+              appointment,
+              service: appointment.service,
+              client: appointment.client,
+            },
+          }),
+      }),
+      activeOpacity: 1,
+    }}
+  >
+    <S.CardContainer>
+      <S.AppointmentInfo>
+        <AppointmentInfo appointment={appointment} />
+      </S.AppointmentInfo>
+      <S.ServicePicture
+        source={{ uri: getFirstPicture(appointment.service || {}) }}
+      />
+    </S.CardContainer>
+  </TouchableOpacity>
 );
 
 Appointment.propTypes = {
   appointment: PropTypes.object.isRequired,
+  openModal: PropTypes.func.isRequired,
 };
 
-export default Appointment;
+export default withOpenModal(Appointment);
